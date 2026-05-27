ac5d38e80620152274c3f6f8b458d135ce746e4b
eugenzor <3356454+eugenzor@users.noreply.github.com>
2026-05-27 18:34:00 +0200

    Add persistant storing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pickle
+
 from colorama import init
 
 from commands import (
@@ -14,49 +16,67 @@ from commands import (
 from models import AddressBook
 from utils import green, red
 
+DATA_FILE = "addressbook.pkl"
+
+
+def save_data(book: AddressBook, filename: str = DATA_FILE) -> None:
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+
+def load_data(filename: str = DATA_FILE) -> AddressBook:
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()
+
 
 def main() -> None:
     init(autoreset=True)
-    book = AddressBook()
+    book = load_data()
     print(green("Welcome to the assistant bot!"))
-    while True:
-        try:
-            user_input = input("Enter a command: ")
-        except (EOFError, KeyboardInterrupt):
-            print()
-            print(green("Good bye!"))
-            break
+    try:
+        while True:
+            try:
+                user_input = input("Enter a command: ")
+            except (EOFError, KeyboardInterrupt):
+                print()
+                print(green("Good bye!"))
+                break
 
-        command, *args = parse_input(user_input)
+            command, *args = parse_input(user_input)
 
-        try:
-            match command:
-                case "close" | "exit":
-                    print(green("Good bye!"))
-                    break
-                case "":
-                    print(red("Please enter a command."))
-                    continue
-                case "hello":
-                    print(green("How can I help you?"))
-                case "add":
-                    print(green(add_contact(args, book)))
-                case "change":
-                    print(green(change_contact(args, book)))
-                case "phone":
-                    print(green(show_phone(args, book)))
-                case "all":
-                    print(green(show_all(args, book)))
-                case "add-birthday":
-                    print(green(add_birthday(args, book)))
-                case "show-birthday":
-                    print(green(show_birthday(args, book)))
-                case "birthdays":
-                    print(green(birthdays(args, book)))
-                case _:
-                    print(red("Invalid command."))
-        except CommandError as exc:
-            print(red(str(exc)))
+            try:
+                match command:
+                    case "close" | "exit":
+                        print(green("Good bye!"))
+                        break
+                    case "":
+                        print(red("Please enter a command."))
+                        continue
+                    case "hello":
+                        print(green("How can I help you?"))
+                    case "add":
+                        print(green(add_contact(args, book)))
+                    case "change":
+                        print(green(change_contact(args, book)))
+                    case "phone":
+                        print(green(show_phone(args, book)))
+                    case "all":
+                        print(green(show_all(args, book)))
+                    case "add-birthday":
+                        print(green(add_birthday(args, book)))
+                    case "show-birthday":
+                        print(green(show_birthday(args, book)))
+                    case "birthdays":
+                        print(green(birthdays(args, book)))
+                    case _:
+                        print(red("Invalid command."))
+            except CommandError as exc:
+                print(red(str(exc)))
+    finally:
+        save_data(book)
 
 
 if __name__ == "__main__":
